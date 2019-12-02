aaebd7dddaf5fc3985f79cd0eefd26ebf871dc0c
Anna Klezovich <belkannkl@gmail.com>
2019-12-02 18:20:19 +0300

    send_file

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from flask import Flask, jsonify, Markup, render_template, request
+from flask import Flask, jsonify, Markup, render_template, request, send_file
 from lxml import etree
 import json
 import konstruktikon_browser
