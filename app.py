c092e114244ced1f8dbb752a506c468ac1b87619
Fyodor Sizov <f.sizov@yandex.ru>
2020-02-07 11:58:19 +0300

    upd

--- app.py
+++ app.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python3
 
 from flask import Flask, jsonify, Markup, render_template, request, send_file, make_response
-from lxml import etree
-from lxml.html import fromstring
-import html
-import json
-import konstruktikon_browser
-import login
-import math
-import os
-import re
-import sqlite_browser
-import urllib.parse
-
-browser = konstruktikon_browser.Browser("konstruktikon2.xml")
 app = Flask(__name__)
 
 
