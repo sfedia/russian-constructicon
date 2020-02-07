42ef1849fd6efc0e8de9d0c0148235fcd2258ece
Fyodor Sizov <f.sizov@yandex.ru>
2020-02-07 20:48:10 +0300

    upd

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, redirect
 import requests
 app = Flask(__name__)
 
 
-@app.route('/<string:route_arg>/')
-def route(route_arg=None, test=None):
-    return url_for('route', **dict(request.args))
+@app.route('/<string:param>', strict_slashes=False)
+def route(param=None, **kwargs):
+    return redirect("http://84.201.175.184:5000/", code=302)
 
+
+@app.route('/', strict_slashes=False)
+def main_f():
+    return redirect("http://84.201.175.184:5000/", code=302)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
