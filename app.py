b09a286884e2155435bb92289f2b1d8314027337
Fyodor Sizov <f.sizov@yandex.ru>
2020-02-07 13:46:22 +0300

    +route_arg

--- app.py
+++ app.py
@@ -5,9 +5,20 @@ import requests
 app = Flask(__name__)
 
 
-@app.route('/<string:route_arg>/', methods=["GET", "POST"])
+@app.route('/<string:route_arg>/')
 def route(route_arg):
-    return url_for("route", **dict(request.args))
+    message = [
+        # route_arg - part of route and argument of our view function
+        'route_arg = %s ' % route_arg,
+        # we can get route_arg from request
+        'route_arg from request = %s ' % request.view_args.get('route_arg'),
+        # request.args - parameters after '?' in url
+        'userID = %s' % request.args.get('userID'),
+        'itemID = %s' % request.args.get('itemID'),
+        # example of url
+        'url with args = %s' % url_for('route', route_arg='info', userID=123, itemID=456)
+    ]
+    return '<br/>'.join(message)
 
 
 if __name__ == "__main__":
