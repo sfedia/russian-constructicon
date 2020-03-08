67dce204bdffb289d1984e3f19e9c60ecebde11d
Fyodor Sizov <f.sizov@yandex.ru>
2020-03-08 14:53:41 +0300

    upd redirection

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 
 @app.route('/<string:param>', strict_slashes=False)
 def route(param=None, **kwargs):
-    return redirect("http://84.201.175.184:5000/", code=302)
+    return redirect("http://office.leosdr.space", code=302)
 
 
 @app.route('/', strict_slashes=False)
 def main_f():
-    return redirect("http://84.201.175.184:5000/", code=302)
+    return redirect("http://office.leosdr.space", code=302)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
